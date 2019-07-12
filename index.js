c3e475b5c9b21dfd19b45c828a743d2e1e756f68
Alexander Riccio <test35965+learn@gmail.com>
2019-07-12 17:18:35 -0400

    solve lab super fast

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
+'use strict'
 // Code your solutions in this file
+function writeCards(messages) {
+    const newArr = [];
+    for (let i = 0; i < messages.length; ++i) {
+        newArr.push(`Thank you, ${messages[i]}, for the wonderful surprise gift!`)
+    }
+    return newArr;
+}
+
+function countdown(num) {
+    while (num >= 0) {
+        console.log(num);
+        num -= 1;
+    }
+}
